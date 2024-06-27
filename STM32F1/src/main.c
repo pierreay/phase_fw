08159487bc60f5802582145173df24ce43e7ee0d
Aurélien Hernandez <aurelien.hernandez@eurecom.fr>
2024-06-27 15:29:16 +0000

    STM32F1 : Removed stdlib printf UART

--- STM32F1/src/main.c
+++ STM32F1/src/main.c
@@ -36,13 +36,6 @@
 UART_HandleTypeDef UartHandle;
 
 /* Private function prototypes -----------------------------------------------*/
-#ifdef __GNUC__
-/* With GCC, small printf (option LD Linker->Libraries->Small printf
-   set to 'Yes') calls __io_putchar() */
-#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
-#else
-#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
-#endif /* __GNUC__ */
 void SystemClock_Config(void);
 static void Error_Handler(void);
 
@@ -100,20 +93,6 @@ int main(void)
   while (1)
   {
   }
-}
-
-/**
-  * @brief  Retargets the C library printf function to the USART.
-  * @param  None
-  * @retval None
-  */
-PUTCHAR_PROTOTYPE
-{
-  /* Place your implementation of fputc here */
-  /* e.g. write a character to the USART1 and Loop until the end of transmission */
-  HAL_UART_Transmit(&UartHandle, (uint8_t *)&ch, 1, 0xFFFF);
-
-  return ch;
 }
 
 /**
